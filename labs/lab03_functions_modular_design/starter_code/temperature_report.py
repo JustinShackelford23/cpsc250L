2832a34360dd67aff04af87d03db6715637aafa9
JustinShackelford23 <justin.shackelford.23@cnu.edu>
2026-06-03 13:26:39 -0400

    Finished Average calculation and Read temperatures

--- labs/lab03_functions_modular_design/starter_code/temperature_report.py
+++ labs/lab03_functions_modular_design/starter_code/temperature_report.py
@@ -1,9 +1,28 @@
 def read_temperatures(filename):
-    pass
-
+    temperatures = []
+    # Formats temperatures as a list
+    with open(filename, "r") as file:
+        for line in file:
+            # reads every line in the file
+            if line != "\n":
+                #detects if the line is not a null
+                number = float(line.strip())
+                # Put the line as a float and removes characters then assigns it to number
+                temperatures.append(number)
+                # Appends number to the list temperature
+    return temperatures
+    # returns the list of temperatures when done with every line
 def calculate_average(values):
-    pass
-
+    Average = 0.0
+    # Assigns average to a float
+    for temp in values:
+        # Reads every float in values
+        Average = Average + temp
+        #adds the float number to avg then loops until finished
+    Avg = Average / len(values)
+    # Does average divided by total numbers in value and assigns to Avg
+    return Avg
+    # Returns Avg
 def find_maximum(values):
     pass
 
@@ -14,6 +33,19 @@ def count_above_threshold(values, threshold):
     pass
 
 def print_report(values):
+    avg = calculate_average(values)
+    max = find_maximum(values)
+    min = find_minimum(values)
+    thresh = count_above_threshold(values,80)
+    # Uses all the functions and assigned them to names avg, max, min, and thresh
+    print("Temperature Report")
+    print("------------------")
+    print("Average temperature:", avg)
+    print("Maximum temperature:", max)
+    print("Minimum temperature:", min)
+    print("Temperatures above 80:", thresh)
+    # Prints the information with the values from the functions
+
     pass
 
 def main():
