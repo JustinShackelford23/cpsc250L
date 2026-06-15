beff870ce915cdf12e7a335f5234bd88bd04543d
JustinShackelford23 <justin.shackelford.23@cnu.edu>
2026-06-15 13:12:13 -0400

    Class Plant, Flower, and Vegetable finished

--- labs/lab09_inheritance_polymorphism/starter_code/plant.py
+++ labs/lab09_inheritance_polymorphism/starter_code/plant.py
@@ -1,31 +1,41 @@
 class Plant:
-    def __init__(self, name, height_cm):
-        pass
+    def __init__(self, name, height_cm=20):
+        self.name = name
+        self.height = height_cm
 
     def care_instructions(self):
-        pass
+        return "Water regularly and provide adequate sunlight."
 
     def __str__(self):
-        pass
+        return f'Name: {self.name}, Height: {self.height} cm, Type: Plant'
 
 
 class Flower(Plant):
-    def __init__(self, name, height_cm, color):
-        pass
+    def __init__(self, name, height_cm=30, color='green'):
+        self.color = color
+        super().__init__(name, height_cm)
 
     def care_instructions(self):
-        pass
+        return "Water regularly, provide full sun, and deadhead spent blooms."
 
     def __str__(self):
-        pass
+        return f'Name: {self.name}, Height: {self.height} cm, Color: {self.color}, Type: Flower'
 
 
 class Vegetable(Plant):
-    def __init__(self, name, height_cm, harvest_days):
-        pass
+    def __init__(self, name, height_cm=10, harvest_days=90):
+        self.harvest = harvest_days
+        super().__init__(name, height_cm)
 
     def care_instructions(self):
-        pass
+        return "Water regularly, provide full sun, and fertilize every two weeks."
 
     def __str__(self):
-        pass
+        return f'Name: {self.name}, Height: {self.height} cm, Harvest Days: {self.harvest}, Type: Vegetable'
+
+
+
+#Commit 1: Add Plant base class
+#Commit 2: Add Flower derived class
+#Commit 3: Add Vegetable derived class
+#Commit 4: Add inventory program and testing
