26c77c1e0a3f8001832313b70dc705eacb77fa22
JustinShackelford23 <justin.shackelford.23@cnu.edu>
2026-06-16 13:08:36 -0400

    Added iterative Fibonacci

--- labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
+++ labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
@@ -12,7 +12,18 @@ def fib_recursive(n)
 
 def fib_iterative(n):
     # TODO: write this function
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        t1 = 0
+        t2 = 1
+        for i in range(2, n+1):
+            t3 = t2 + t1
+            t1 = t2
+            t2 = t3
+        return t3
 
 
 def time_function(function, n):
