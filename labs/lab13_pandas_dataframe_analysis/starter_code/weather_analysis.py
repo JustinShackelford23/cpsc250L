cbd1b19d69b929a261617da87043cef81dde19fb
JustinShackelford23 <justin.shackelford.23@cnu.edu>
2026-06-23 14:03:43 -0400

    All calculations finished

--- labs/lab13_pandas_dataframe_analysis/starter_code/weather_analysis.py
+++ labs/lab13_pandas_dataframe_analysis/starter_code/weather_analysis.py
@@ -5,29 +5,49 @@ import matplotlib.pyplot as plt
 def load_weather_data(filename):
     # TODO: load the data from csv file
     # TODO: return a Pandas dataframe
-    pass
+    dataframe = pd.read_csv(filename)
+    return dataframe
 
 
 def print_summary(df):
     # TODO: print summary statistics
     # TODO: extract the mean temperature and print it
-    pass
+    print(df.describe())
+    print()
+    print (f'Mean: {(df["high C"].mean() + df["low C"].mean()) /2}')
+
 
 def add_celsius(df):
     # TODO: add columns for temperatures converted to Celsius
     # TODO: return modified dataframe
-    pass
-
+    df['high C'] = (df["high"] - 32) / 1.8
+    df['low C'] = (df['low'] - 32) / 1.8
+    return df
 
 def clean_temperature_range(df, t_low_cut, t_high_cut):
     # TODO: remove days where T_low < t_low_cut or T_high > t_high_cut
     # TODO: return modified dataframe
-    pass
+    df = df[df["high C"] <= t_high_cut]
+    df = df[df["low C"] >= t_low_cut]
+    return df
+
+
+
+
+
+
 
 
 def plot_temperatures(df):
     # TODO: plot both high and low temperatures on the same graph
-    pass
+    plt.plot(df["high C"], label="high")
+    plt.plot(df["low C"], label="low")
+    plt.title('Weather Data')
+    plt.xlabel('Day')
+    plt.ylabel('Temperature (C)')
+    plt.legend()
+    plt.show()
+
 
 
 def main():
@@ -47,3 +67,7 @@ def main():
     plot_temperatures(dataframe)
 
 main()
+#Commit 1: Load and inspect DataFrame
+#Commit 2: Add summary statistics
+#Commit 3: Add filtering and calculated columns
+#Commit 4: Add plot and cleanup
